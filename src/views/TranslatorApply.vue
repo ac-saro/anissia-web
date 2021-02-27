7f72586ff53e911889e673cee07fc739efd31eba
PARK Yong Seo <j@saro.me>
2021-02-27 20:05:26 +0900

    translator-apply -- apply layout

--- src/views/TranslatorApply.vue
+++ src/views/TranslatorApply.vue
@@ -4,8 +4,61 @@
       <div class="basic-layout-mono" id="tr-apply">
         <div class="base-mat">
 
-          <div class="doc-title">자막</div>
+          <div class="doc-title fo">
+            <div class="fl">자막제작자 신청</div>
+            <div v-if="applyNo == -1 && hasPer('apply')" class="fr"><input type="button" class="std-inp-btn" value="글쓰기" @click="$router.push('?applyNo=0')"/></div>
+            <div v-if="applyNo > 0" class="fr"><input type="button" class="std-inp-btn" value="목록" @click="$router.push($route.path)"/></div>
+          </div>
 
+          <!-- apply -->
+          <div v-if="applyNo == 0" class="app">
+            <div class="agree">
+              <div class="subject">자막제작자 신청 동의사항</div>
+              <ul>
+                <li>권한신청을 위해서 4편이상(동일작품)의 자막 작업물이 있어야 합니다.</li>
+                <li>블로그 내에 불법영상이나 불건전한 정보가 있을 경우 거부될 수 있습니다.</li>
+                <li>권한 부여 후에도 위와 같은 사항이 발견되면 고지 없이 권한이 회수될 수 있습니다.</li>
+                <li>자막 도용시 추후에 적발되더라도 권한이 회수될 수 있습니다.</li>
+              </ul>
+              <div class="sign">
+                <label><input type="checkbox" /> 위 사항을 읽고 확인하였습니다.</label>
+              </div>
+            </div>
+
+            <div class="report">
+              <div class="subject">블로그주소</div>
+              <div class="pad">
+                <input type="text" class="std-inp-txt basic-border-color" placeholder="http://example-blog.com - 동일작 기준 4편이상 자막 확인을 위한 블로그 주소 입력" />
+              </div>
+
+            </div>
+
+            <div class="submit">
+              <input class="std-inp-btn" type="button" value="신청하기"/>
+            </div>
+          </div>
+          <!-- view -->
+          <div v-if="applyNo != 0" class="view">
+            <div class="subject">자막제작자 신청 진행사항</div>
+            <div>
+              <table class="report">
+                <tr><th>심사번호</th><td>11</td></tr>
+                <tr><th>상태</th><td>11</td></tr>
+                <tr><th>신청자</th><td>11</td></tr>
+                <tr><th>블로그주소</th><td>11</td></tr>
+              </table>
+
+            </div>
+
+            <div>
+              <table>
+                <tr>
+                  <td>의견</td>
+                  <td>박용서: 가나다라마바바사</td>
+                </tr>
+              </table>
+            </div>
+          </div>
 
           <!-- list -->
           <div>
@@ -57,6 +110,11 @@ import AnissiaUtil from "@/utils/AnissiaUtil";
     AnimeRank,
     Pagination
   },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  },
   created() {
     this.load();
   },
@@ -67,7 +125,7 @@ import AnissiaUtil from "@/utils/AnissiaUtil";
   },
   methods: {
     load() {
-      const animeNo = this.animeNo = Nabi.address().getIntParameter("animeNo");
+      const applyNo = this.applyNo = Nabi.address().getIntParameter("applyNo");
       this.query = (Nabi.address().getParameter("q") || '').trim();
       this.page = Math.max(Nabi.address().getIntParameter("page"), 1) - 1;
       const pageQuery = `${this.page} ${this.query}`;
@@ -78,8 +136,8 @@ import AnissiaUtil from "@/utils/AnissiaUtil";
       }
 
       // view
-      if (animeNo > 0) {
-        AnimeService.getAnime(animeNo, anime => {
+      if (applyNo > 0) {
+        AnimeService.getAnime(applyNo, anime => {
           AnimeService.toInfo(anime);
           this.anime = anime;
         });
@@ -95,6 +153,15 @@ import AnissiaUtil from "@/utils/AnissiaUtil";
           this.list = list;
         });
       }
+    },
+    hasPer(action: string, node: (any | null) = null) {
+      const user = this.user;
+      if (!user.isLogin) { return false; }
+      return true;
+      // switch (action) {
+      //   case 'apply': return !user.isAdmin;
+      //
+      // }
     },
     autocorrect(event: KeyboardEvent) {
       const word = (event.target as any).value;
@@ -131,7 +198,7 @@ import AnissiaUtil from "@/utils/AnissiaUtil";
       }
     },
     clickAutocorrect(event: MouseEvent) {
-      this.autoOn = Nabi.matchesParents(event.target, ['#anime .search']) != '';
+      this.autoOn = Nabi.matchesParents(event.target, ['#tr-apply .search']) != '';
     },
     hrefList(animeNo: number) {
       return Nabi.address().setParameter('animeNo', animeNo).href;
@@ -153,8 +220,8 @@ import AnissiaUtil from "@/utils/AnissiaUtil";
 export default class TranslatorApply extends Vue {
   data() {
     return {
-      anime: null as any,
-      animeNo: -1,
+      view: null as any,
+      applyNo: -1,
       query: '',
       autoQuery: '',
       autoList: [],
@@ -170,50 +237,74 @@ export default class TranslatorApply extends Vue {
 
 <style>
 
-#anime .base-mat { padding: 8px; }
-#anime .anime-view-error { font-size:24px; text-align: center; line-height: 2; margin:50px 0 70px; }
-#anime .anime-empty { text-align: center; padding: 200px 0; }
+#tr-apply .base-mat { padding: 8px; }
+#tr-apply .fo { overflow: auto; }
+#tr-apply .fl { float:left; }
+#tr-apply .fr { float:right; text-align: right }
+#tr-apply .doc-title { padding:0 !important; overflow: auto }
+#tr-apply .doc-title .fl { padding: 6px 8px 10px; }
+#tr-apply .doc-title .fr { padding:5px 4px 0 0; }
+#tr-apply .doc-title input { border-width:0; height:32px; }
+#tr-apply .app .subject { font-size:18px; font-weight: bold; padding:18px 4px 10px; }
+#tr-apply .app input[type=text],
+#tr-apply .app input[type=button]{ height: 40px; }
+#tr-apply .app .agree ul { padding: 0 0 0 28px; margin:0; }
+#tr-apply .app .agree ul li { font-size:14px; line-height:1.8 }
+#tr-apply .app .agree .sign { padding:16px 4px; font-size:14px; }
+#tr-apply .app .report .pad { padding:0 4px; }
+#tr-apply .app .report input { width:100%; padding:0 8px; border-width:1px; }
+#tr-apply .app .report .info { font-size:14px; opacity: .8 }
+#tr-apply .app .submit { padding:36px 4px 48px; }
+#tr-apply .app .submit input { padding:0 24px; border:0; }
 
-#anime table { }
-#anime table.list { margin-top:6px;  width:100% }
-#anime table.list th { height:40px;  }
-#anime table.list td { font-size:13px; padding:10px 4px; border-top-width: 1px; line-height: 1.5 }
-#anime table.list td.anime-no { text-align: center; width:60px; }
-#anime table.list td div.subject { font-size:15px; padding-top:2px; }
-#anime table.list td div.info { padding:4px 0 2px; }
-#anime table.list tr.sel td { font-weight: bold }
+#tr-apply .view .subject { font-size:18px; font-weight: bold; padding:18px 4px 10px; }
+#tr-apply .view table.report { min-width:40%; margin: 12px 0 20px; }
+#tr-apply .view table.report th { width:100px; text-align: left }
+#tr-apply .view table.report th,
+#tr-apply .view table.report td{ border-width: 1px; padding: 8px 12px }
 
-#anime .view { padding: 8px 8px 40px; }
-#anime .view table.view-info { min-width:400px }
-#anime .view .title { line-height: 2; font-size:20px; font-weight: bold; padding:8px 2px 12px; }
-#anime .view .in-tag:not(:first-child):before { content: ', ' }
-#anime .view table.basic-info tr.basic-info-tr > td { border-width: 1px; line-height: 32px; padding:8px 16px }
-#anime .view .caption-title { line-height: 2; font-size:18px; font-weight: bold; padding:14px 2px 12px; }
-#anime .view .caption table th,
-#anime .view .caption table td { border-width: 1px; padding:8px 16px; text-align: center }
 
-#anime .search { padding: 40px 40px; }
-#anime .search .search-box { }
-#anime .search .search-box input { width:100%; height:40px; padding:0 8px; font-size:16px; }
-#anime .search .autocorrect { height:0; font-size:15px; }
-#anime .search .autocorrect .autocorrect-box { position: relative; backdrop-filter:blur(3px);border-width: 0 1px 1px;}
-#anime .search .autocorrect div.node { padding:8px 12px; }
-#anime .search .autocorrect div.node.sel,
-#anime .search .autocorrect div.node:hover { font-weight: bold; }
 
-html.light #anime .search .autocorrect .autocorrect-box { background: rgba(255, 255, 255, .7); border:1px solid #eee; }
-html.light #anime .search .autocorrect div.node span { color:#2f7cbd }
-html.light #anime table.list a { color:#444; text-decoration: none }
+#tr-apply table { }
+#tr-apply table.list { margin-top:6px;  width:100% }
+#tr-apply table.list th { height:40px;  }
+#tr-apply table.list td { font-size:13px; padding:10px 4px; border-top-width: 1px; line-height: 1.5 }
+#tr-apply table.list td.anime-no { text-align: center; width:60px; }
+#tr-apply table.list td div.subject { font-size:15px; padding-top:2px; }
+#tr-apply table.list td div.info { padding:4px 0 2px; }
+#tr-apply table.list tr.sel td { font-weight: bold }
 
-html.dark #anime .search .autocorrect .autocorrect-box { background: rgba(0, 0, 0, .7); border:1px solid #333; }
-html.dark #anime .search .autocorrect div.node span { color:#2e7bb5 }
-html.dark #anime table.list a { color:#aaa; text-decoration: none }
+#tr-apply .view { padding: 8px 8px 40px; }
+#tr-apply .view table.view-info { min-width:400px }
+#tr-apply .view .title { line-height: 2; font-size:20px; font-weight: bold; padding:8px 2px 12px; }
+#tr-apply .view .in-tag:not(:first-child):before { content: ', ' }
+#tr-apply .view table.basic-info tr.basic-info-tr > td { border-width: 1px; line-height: 32px; padding:8px 16px }
+#tr-apply .view .caption-title { line-height: 2; font-size:18px; font-weight: bold; padding:14px 2px 12px; }
+#tr-apply .view .caption table th,
+#tr-apply .view .caption table td { border-width: 1px; padding:8px 16px; text-align: center }
+
+#tr-apply .search { padding: 40px 40px; }
+#tr-apply .search .search-box { }
+#tr-apply .search .search-box input { width:100%; height:40px; padding:0 8px; font-size:16px; }
+#tr-apply .search .autocorrect { height:0; font-size:15px; }
+#tr-apply .search .autocorrect .autocorrect-box { position: relative; backdrop-filter:blur(3px);border-width: 0 1px 1px;}
+#tr-apply .search .autocorrect div.node { padding:8px 12px; }
+#tr-apply .search .autocorrect div.node.sel,
+#tr-apply .search .autocorrect div.node:hover { font-weight: bold; }
+
+html.light #tr-apply .search .autocorrect .autocorrect-box { background: rgba(255, 255, 255, .7); border:1px solid #eee; }
+html.light #tr-apply .search .autocorrect div.node span { color:#2f7cbd }
+html.light #tr-apply table.list a { color:#444; text-decoration: none }
+
+html.dark #tr-apply .search .autocorrect .autocorrect-box { background: rgba(0, 0, 0, .7); border:1px solid #333; }
+html.dark #tr-apply .search .autocorrect div.node span { color:#2e7bb5 }
+html.dark #tr-apply table.list a { color:#aaa; text-decoration: none }
 
 @media (max-width: 800px) {
-  #anime .mob-hide { display: none; }
-  #anime .search { padding:10px 0 6px; }
-  #anime .search .autocorrect div.node { padding:16px; }
-  #anime .view table.view-info { width:100% }
+  #tr-apply .mob-hide { display: none; }
+  #tr-apply .search { padding:10px 0 6px; }
+  #tr-apply .search .autocorrect div.node { padding:16px; }
+  #tr-apply .view table.view-info { width:100% }
 }
 
 </style>
