658a4cd9cca080f6ccdd3bd7fbad8d26840205be
PARK Yong Seo <j@saro.me>
2021-03-11 23:57:01 +0900

    account ready

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <div class="basic-layout">
-      <div id="introduce" class="basic-layout-mono">
+      <div id="account" class="basic-layout-mono">
         <div class="base-mat">
+
+          계정
 
 <pre style="font-size:16px; line-height: 2">
 제작중 (아래 기능 추가예정)
@@ -10,6 +12,9 @@
 회원정보 / 수정
 ------
 이메일 바꾸기 (계정)
+
+
+
 </pre>
 
         </div>
@@ -30,3 +35,7 @@ import {Options, Vue} from "vue-class-component";
 export default class Account extends Vue {
 }
 </script>
+
+<style>
+#account .base-mat { padding: 8px; }
+</style>
