70ddd0f73820b7d5407a74cbea660cf4e0bc97ce
PARK Yong Seo <j@saro.me>
2021-05-07 21:33:04 +0900

    create anime caption recent

---
+++ src/components/AnimeCaptionRecent.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="anime-caption-recent a-text-style">
+    <table class="rank-tab">
+      <tr>
+        <td><div class="basic-border-color" :class="{'select': rank.period === 'week'}" @click="selectRankType('week')">주간</div></td>
+        <td><div class="basic-border-color" :class="{'select': rank.period === 'month'}" @click="selectRankType('month')">월간</div></td>
+        <td><div class="basic-border-color" :class="{'select': rank.period === 'quarter'}" @click="selectRankType('quarter')">분기</div></td>
+      </tr>
+    </table>
+    <div class="rank-pad">
+      <table class="rank-table">
+        <tr v-for="node in rank.list" :key="node.animeNo" :class="({'empty-list': node.animeNo == 0})">
+          <td class="rank">{{node.rank}}</td>
+          <td class="subject">
+            <router-link v-if="node.animeNo > 0" :to="`/anime?animeNo=${node.animeNo}`">{{node.subject}}</router-link>
+            <div v-else>-</div>
+          </td>
+          <td class="diff">
+            <div v-if="node.animeNo == 0 || node.diff == 0"> </div>
+            <div v-else-if="node.diff == null" class="diff-up diff-new fas fa-angle-double-up" title="#100 신규진입"></div>
+            <div v-else-if="node.diff > 0" class="diff-up">▲ {{node.diff}}</div>
+            <div v-else-if="node.diff < 0" class="diff-down">▼ {{-node.diff}}</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import AnimeService from "@/service/AnimeService";
+
+@Options({
+  props: {
+    /**
+     * "dynamic" or undefined (default value)
+     * - after created, call the api automatically
+     * "static"
+     * - after created, do not call the api automatically
+     */
+    mode: String
+  },
+  created() {
+    if (this.mode != 'static' && this.rank.list.length == 0) {
+      this.selectRankType('week');
+    }
+  },
+  computed: {
+    rank() {
+      return this.$store.state.rank;
+    }
+  },
+  methods: {
+    selectRankType(type: string) {
+      this.rank.period = type;
+      if (!this.rank[type].length) {
+        AnimeService.getRank(type, (list) => this.rank.list = this.rank[type] = list);
+      } else {
+        this.rank.list = this.rank[type];
+      }
+    }
+  },
+})
+export default class AnimeCaptionRecent extends Vue {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+.anime-caption-recent {  }
+.anime-caption-recent table { width:100%; }
+.anime-caption-recent .rank-tab td { width:33.33%; }
+.anime-caption-recent .rank-tab td div { text-align: center; line-height: 48px; cursor: pointer; font-size:15px; }
+.anime-caption-recent .rank-table td,
+.anime-caption-recent .rank-tab td div { border-bottom-width: 1px; }
+.anime-caption-recent .rank-pad { padding:4px 0 0; }
+.anime-caption-recent .rank-table td { height: 36px; padding: 6px 0; line-height: 1.6; font-size:14px }
+.anime-caption-recent .rank-table td.rank { min-width:20px; max-width: 50px; text-align: center; padding:0 16px; font-weight: bold }
+.anime-caption-recent .rank-table td.subject { width:90%; }
+.anime-caption-recent .rank-table td.diff { min-width:50px; text-align: right; padding-right:16px; font-weight: bold }
+.anime-caption-recent .rank-table td.diff .diff-new { font-size:16px; }
+.anime-caption-recent .rank-table tr.empty-list { opacity: .3 }
+
+@media (max-width: 1023px) {
+  .anime-caption-recent .rank-table tr:nth-child( n + 11 ) { display: none }
+}
+
+html.light .anime-caption-recent .rank-table td.diff .diff-up { color:#d45e5e }
+html.light .anime-caption-recent .rank-table td.diff .diff-down { color:#7490c3 }
+html.light .anime-caption-recent .rank-table td.diff .diff-new { color:#e03f3f }
+html.light .anime-caption-recent .rank-tab td div.select { border-color: #276998 }
+html.light .anime-caption-recent a { color:#444 }
+
+html.dark .anime-caption-recent .rank-table td.diff .diff-up { color:#4c2626 }
+html.dark .anime-caption-recent .rank-table td.diff .diff-down { color:#132046 }
+html.dark .anime-caption-recent .rank-table td.diff .diff-new { color:#ca3838 }
+html.dark .anime-caption-recent .rank-tab td div.select { border-color: #aaa }
+html.dark .anime-caption-recent a { color:#aaa }
+</style>
