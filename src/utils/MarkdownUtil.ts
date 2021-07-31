dc768a876c6863f4f66e34b7967816fb208e8b8c
PARK Yong Seo <j@saro.me>
2021-07-31 21:15:49 +0900

    version up and md breaks update

--- src/utils/MarkdownUtil.ts
+++ src/utils/MarkdownUtil.ts
@@ -5,17 +5,13 @@ export default class MarkdownUtil {
   public static md = require('markdown-it')({
     html:         false,
     xhtmlOut:     false,
-    breaks:       false,
+    breaks:       true,
     linkify:      false,
     typographer:  false,
     highlight(code: string, lang: string) { return "코드를 지원하지 않습니다."; },
   });
 
   public static render(text: string): string {
-    const rv = (MarkdownUtil.md.render(text) as string)
-      .split(/&lt;br[ ]?\/&gt;/g).join('<br/>')
-      .replace(/[\r]+/g, "")
-      .split(/[\n]{1,2}/g).join('<br/>');
-    return rv;
+    return MarkdownUtil.md.render(text) as string;
   }
 }
